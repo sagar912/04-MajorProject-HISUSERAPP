6d918dca2292dd7d21577686816c1a62eefe61e0
sagar912 <66301640+sagar912@users.noreply.github.com>
2020-09-22 19:43:33 +0530

    Update AppConstants.java

--- src/main/java/com/sagar/ARmodule/constants/AppConstants.java
+++ src/main/java/com/sagar/ARmodule/constants/AppConstants.java
@@ -2,7 +2,7 @@ package com.sagar.ARmodule.constants;
 
 public class AppConstants {
 	
-	public static final String LOCKED_STR="LOCKEDd";
+	public static final String LOCKED_STR="LOCKED";
 	
 	public static final String ISDELETED_STR="NO";
 	
